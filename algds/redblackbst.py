ba934a9b4a92d4ff9806b7a31f49cf1f9e951af0
elkurto <kurt.buehner@gmail.com>
2025-03-14 06:48:27 -0400

    updated test_redblackbst.py

--- algds/redblackbst.py
+++ algds/redblackbst.py
@@ -299,34 +299,3 @@ class Rbtree(object):
 		self._to_list( h.right, values )
 
 
-class TestRbtree(unittest.TestCase):
-
-	def test_put_get_001(self):
-		rbtree =Rbtree()
-		rbtree.put(1,'A')
-		node =rbtree.get(1)
-		self.assertEqual( node.value, 'A')
-
-	def test_put_get_002(self):
-		rbtree =Rbtree()
-		rbtree.put(1,'A')
-		rbtree.put(3,'C')
-		rbtree.put(4,'D')
-		
-		node =rbtree.get(1)
-		self.assertEqual( node.value, 'A')
-		
-		node_003 =rbtree.get(3)
-		self.assertEqual( node_003.value, 'C')
-
-		node_004 =rbtree.get(4)
-		self.assertEqual( node_004.value, 'D')
-
-		node_005 =rbtree.get(5)
-		self.assertIsNone(node_005)
-		
-		
-if __name__ == "__main__":
-	#import sys;sys.argv = ['', 'Test.testName']
-	unittest.main()
-	
