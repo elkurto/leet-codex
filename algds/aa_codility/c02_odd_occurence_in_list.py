242cda589afedc35ce420792926a339c5254fbad
elkurto <kurt.buehner@gmail.com>
2025-04-09 06:53:42 -0400

    added algds/aa_codility/c02_odd_occurence_in_list.py

---
+++ algds/aa_codility/c02_odd_occurence_in_list.py
@@ -0,0 +1,24 @@
+import sys
+from collections import defaultdict
+
+"""
+Return a list of values in :param:list that occur an odd number of times.
+e.g.   find_unpaired_elements_in_list( [3,9,7,3,9,6] ) 
+   // returns [7,6] 
+"""
+def find_unpaired_elements_in_list(ary):
+  s =set()
+  for element in ary:
+    if element in s:
+      s.remove(element)
+    else:
+      s.add(element)
+
+  return list(s)
+
+
+if __name__ == "__main__":
+  #ary =[3,9,7,3,9,6]
+  #ary_unpaired_element =find_unpaired_elements_in_list(ary)  # returns [7,6]
+  ary_unpaired_element =find_unpaired_elements_in_list(sys.argv[1:])
+  print( 'unpaired_elements ={0}'.format( ary_unpaired_element ))
