140b55fbd6a735f8e7723824e9b534471c7d1fcb
elkurto <kurt.buehner@gmail.com>
2025-07-27 11:23:51 -0400

    added tests

--- algds/balanced_tree_234/__test__/test_stack_fixed_type_size.py
+++ algds/balanced_tree_234/__test__/test_stack_fixed_type_size.py
@@ -16,6 +16,36 @@ class TestStackFixedTypeAndSize(unittest.TestCase):
     s.append(3)
     s.append(4)
     self.assertEqual( s[0] , 3)
+    self.assertEqual( s[1] , 4)
+
+  def test_set_index(self):
+    s =StackFixedTypeAndSize(int, 5)
+    s[0] =0
+    s[1] =1
+    s[2] =2
+
+    self.assertEqual( s[0], 0)
+    self.assertEqual( s[1], 1)
+    self.assertEqual( s[2], 2)
+
+    self.assertRaises(IndexError, s.set_element_at_index, 4, 1000)
+    self.assertRaises(IndexError, s.set_element_at_index, 5, 1000)
+
+    rval =s.set_element_at_index(2,222)
+    self.assertEqual( 2, rval)
+
+  def test_pop(self):
+    s =StackFixedTypeAndSize(int, 5)
+    s[0] =0
+    s[1] =1
+    s[2] =2
+
+    self.assertEqual( s.pop(), 2)
+    self.assertEqual( s.pop(), 1)
+    self.assertEqual( s.pop(), 0)
+
+
+
 
 if __name__ == "__main__":
   unittest.main()
--- algds/balanced_tree_234/stack_fixed_type_size.py
+++ algds/balanced_tree_234/stack_fixed_type_size.py
@@ -8,10 +8,9 @@ class StackFixedTypeAndSize:
 
   def append(self, element):
     if self.population >= self.maxlen:
-      raise Exception(f"Exception idx ={idx} is invalid :: 0 <= idx < maxlen ({self.maxlen})")
+      raise Exception(f"Exception fixed-sized-stack is full :: population={self.population} :: maxlen=({self.maxlen})")
     if not isinstance(element, self.clazz):
       raise Exception('element is not an instance of '+ self.clazz)
-
 
     self.ary[self.population] =element
     self.population +=1
@@ -28,16 +27,33 @@ class StackFixedTypeAndSize:
   def __getitem__(self, idx):
     return self.__index__(idx)
 
-  def __setitme__(self, idx, new_item):
+  def __setitem__(self, idx, new_item):
+    return self.set_element_at_index(idx, new_item)
+
+  def set_element_at_index(self, idx, new_item):
     old_item =None
-    if 0 <= idx < self.population:
-      old_item =self.ary[idx] =new_item
+    if 0 <= idx < self.population and idx < self.maxlen:
+      # then replace existing element
+      old_item =self.ary[idx]
+      self.ary[idx] =new_item
+    elif idx == self.population:
+      self.append(new_item)
+    else:
+      raise IndexError( f"IndexError: idx={idx} must be in [0,population] == [0,{self.population} and idx < maxlen({self.maxlen})")
+
     return old_item
 
-  def pop(self, idx):
+  def pop(self, idx=None):
     item =None
-    if 0 <= idx < self.population:
+    if idx is None:
+      idx =self.population -1  # compute last index
+
+    if 0 <= idx < self.population: # remove item at last index
       item =self.ary.pop(idx)
       self.population -=1
+
     return item
+
+  def push(self, element):
+    return self.append(element)
 
